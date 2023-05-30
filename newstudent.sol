40e5ffcbd874112d7a9a8381f428e0b9262c2d90
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-05-30 16:29:08 +0530

    newstudent.sol

---
+++ newstudent.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20 ;
+import "./student.sol" ;
+
+contract employer {
+
+    studentContract s1 = studentContract(0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47);
+    
+    function checkVerifyPassOut(uint _roll) view public returns (string memory) {
+
+        if (s1.CheckBG(_roll)) 
+          return "student is Passed Out." ;
+             
+        else
+        return "student did NOT Pass OUT." ;
+       
+    }
+}
